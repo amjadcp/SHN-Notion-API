af6bc3c4bb93e8405964fba384576ed11346285d
Your Name <you@example.com>
2022-05-07 18:59:26 +0530

    a

--- index.js
+++ index.js
@@ -17,28 +17,26 @@ function notionPropertiesById(properties) {
   const myPage = await notion.databases.query({
     database_id: "2beabde0c63f435aaf163d55f57654ee",
   });
-  console.log(myPage);
-  console.log();
   console.log(
-    notionPropertiesById(myPage.results[0].properties)
+    notionPropertiesById(myPage.results.length)
   );
 })();
 
-function fromNotionObject(notionPage) {
-  const propertiesById = notionPropertiesById(notionPage.properties);
+// function fromNotionObject(notionPage) {
+//   const propertiesById = notionPropertiesById(notionPage.properties);
 
-  return {
-    id: notionPage.id,
-    title: propertiesById[process.env.NOTION_TITLE_ID].title[0].plain_text,
-    votes: propertiesById[process.env.NOTION_VOTES_ID].number,
-    tags: propertiesById[process.env.NOTION_TAGS_ID].multi_select.map(
-      (option) => {
-        return { id: option.id, name: option.name };
-      }
-    ),
-    isProject: propertiesById[process.env.NOTION_PROJECT_ID].checkbox,
-    description:
-      propertiesById[process.env.NOTION_DESCRIPTION_ID].rich_text[0].text
-        .content,
-  };
-}
+//   return {
+//     id: notionPage.id,
+//     title: propertiesById[process.env.NOTION_TITLE_ID].title[0].plain_text,
+//     votes: propertiesById[process.env.NOTION_VOTES_ID].number,
+//     tags: propertiesById[process.env.NOTION_TAGS_ID].multi_select.map(
+//       (option) => {
+//         return { id: option.id, name: option.name };
+//       }
+//     ),
+//     isProject: propertiesById[process.env.NOTION_PROJECT_ID].checkbox,
+//     description:
+//       propertiesById[process.env.NOTION_DESCRIPTION_ID].rich_text[0].text
+//         .content,
+//   };
+// }
